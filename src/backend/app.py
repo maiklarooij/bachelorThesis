13fc220d6c62c0e912dca066f4480dc65383c7f4
deboradum <pvwpvw@protonmail.com>
2024-04-14 08:03:30 +0200

    more verbosity in server

--- src/backend/app.py
+++ src/backend/app.py
@@ -11,17 +11,26 @@ from WhisperClient import MLX_Transcriber, Torch_Transcriber
 
 load_dotenv()
 
+device = "mps" if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else "cpu"
+
 # TODO: check if cuda/ mlx is available and initialise based on that
 whisper_client = None
-if torch.backends.mps.is_available():
+if device == "mps":
+    print("Loading whisper MLX client")
     whisper_client = MLX_Transcriber(os.environ.get("MLX_WHISPER_MODEL"))
 else:
+    print("Loading whisper Torch client")
     whisper_client = Torch_Transcriber()
 transcribing = False
 
-weaviate_client = None
-embed_client = None
-pyannote_client = None
+print("Loading pyannote client")
+pyannote_client = Pyannote(device)
+
+print("Loading Weaviate client")
+weaviate_client = Weaviate()
+
+print("Loading embedding client")
+embed_client = SFRMistralEmbedder()
 
 app = FastAPI()
 
