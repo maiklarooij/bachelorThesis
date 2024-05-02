14d17b8ec5f508ed2e2204dea728657ab60cdc17
deboradum <pvwpvw@protonmail.com>
2024-05-02 11:54:15 +0200

    frontend configs

---
+++ src/frontend/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig, searchForWorkspaceRoot } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from "path"
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    root: "/Volumes/Drive/data/",
+  },
+  plugins: [vue()],
+});
