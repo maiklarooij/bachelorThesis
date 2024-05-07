b408d1a8f2ed0d7b745be4a271b2f34d3871311f
deboradum <pvwpvw@protonmail.com>
2024-05-07 16:38:26 +0200

    highlight search text in results

--- src/frontend/src/views/Search.vue
+++ src/frontend/src/views/Search.vue
@@ -5,6 +5,7 @@ import SearchBar from "../components/SearchBar.vue"
 import SearchSettings from '../components/SearchSettings.vue';
 import Results from '../components/Results.vue';
 
+const searchQuery = ref("")
 const gemeentes = ref([]);
 const searchAlpha = ref(0.5);
 const searchLimit = ref(10);
@@ -55,18 +56,23 @@ function updateSearchAlpha(emittedValue) {
     searchAlpha.value = emittedValue
 }
 
+function HandleNewQueryEmit(query) {
+    console.log("New query is", query)
+    searchQuery.value = query
+}
+
 </script>
 
 <template>
     <div class="flex flex-col">
         <SearchBar :limit="searchLimit" :alpha="searchAlpha" :method="searchMethod" :gemeente="searchGemeente"
             :type="searchType" :year="searchYear" :speaker="searchSpeaker" :video="searchVideoID"
-            @newResults="updateResults" />
+            @newResults="updateResults" @newQuery="HandleNewQueryEmit" />
         <SearchSettings :searchGemeenteActive="true" :searchTypeActive="true" :searchYearActive="true"
             :availableSpeakersProp="[]" :gemeentes="gemeentes" @newGemeenteToSearch="updateSearchGemeente"
             @newMeetingTypeToSearch="updateSearchType" @newMeetingYearToSearch="updateSearchYear"
             @newSpeakerToSearch="updateSearchSpeaker" @newSearchMethod="updateSearchMethod"
             @newSearchLimit="updateSearchLimit" @newSearchalpha="updateSearchAlpha" />
-        <Results :results="searchResults"></Results>
+        <Results :query="searchQuery" :results="searchResults"></Results>
     </div>
 </template>
